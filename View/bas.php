d14c83b1a9c14ce248eef30a385525ce7f843ee6
sali6000 <34247058+sali6000@users.noreply.github.com>
2018-05-14 15:35:07 +0200

    MAJ 2

--- View/bas.php
+++ View/bas.php
@@ -2,7 +2,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<footer class="col-lg-12">
-				Fait par DE GUGLIELMO Roberto 
+				Fait par Steven et Thomas
 				<a href="#haut" >haut de page</a>
 			</footer>
 		</div>
