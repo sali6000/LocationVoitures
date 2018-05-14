e363582ec656f7ddea405d2b5d0542b8ae94994e
sali6000 <34247058+sali6000@users.noreply.github.com>
2018-05-14 17:52:07 +0200

    MAJ correction Login

--- Controller/Utilitaires.php
+++ Controller/Utilitaires.php
@@ -6,8 +6,7 @@ class Utilitaires
 		$utilisateurs=Model::load("utilisateurs");
 		$utilisateurs->id=$pseudo;
 		$utilisateurs->read();
-
-		if($utilisateurs->data[0]->code==$motDePasse)
+		if($utilisateurs->data[0]->code==$motDePasse && $utilisateurs->data[0]->actif == 1)
 		{
 			return true;
 		}
