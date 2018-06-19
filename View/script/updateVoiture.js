58f7321d2d52a9036565c03238b7db210b0c5b13
sali6000 <34247058+sali6000@users.noreply.github.com>
2018-06-19 22:46:28 +0200

    update

--- View/script/updateVoiture.js
+++ View/script/updateVoiture.js
@@ -1,21 +1,20 @@
 // Cette méthode jquery permet de traité les informations de façons synchronisée (AJAX)
 // C'est à dire qu'il n'y a pas de rechargement de page lorsque je souhaite accéder au serveur
 //
-// Autrement dit, cette méthode jquery permet de faire de l'ajax et donc de ne pas avoir à 
+// Autrement dit, cette méthode jquery permet de faire de l'ajax et donc de ne pas avoir à
 // rafraichir la page sur laquel je souhaite effectuer un changement depuis le serveur
 $(document).ready(function()
 {
     $("#submitVoiture").click(function(event)
     {
-        // Assigner les valeurs des attributs provenant d'un formulaire PHP dans des variables JVS 
+        // Assigner les valeurs des attributs provenant d'un formulaire PHP dans des variables JVS
         // dans ce cas çi est nécessaire car nous sommes dans du JVS.
         //
         // (La méthode POST permet de ne pas afficher les valeurs dans le protocole HTTP (URL))
         //
         // Les attributs venant d'une méthode POST en PHP doivent donc obligatoirement être assignées
-        // dans des variables JVS contrairement à la méthode GET qui permet à du javascript 
+        // dans des variables JVS contrairement à la méthode GET qui permet à du javascript
         // de récupérer directement les valeurs provenant d'un formulaire PHP.
-        S
         var marque = $("#isMarque").val();
         var modele = $("#isModele").val();
         var couleur = $("#isCouleur").val();
@@ -30,14 +29,14 @@ $(document).ready(function()
             '#', // La destination du POST
             {
                 // Nous créons les attributs; ex: $_POST('utilisateur') avec les valeurs
-                // qu'ils doivent contenir 
+                // qu'ils doivent contenir
 
                 marque : marque,
                 modele : modele,
                 couleur : couleur,
                 date_location : date_location,
                 date_retour : date_retour,
-                actif : actif
+                actif : actif,
             },
             function(data)
             {
@@ -52,14 +51,14 @@ $(document).ready(function()
                      $("#resultatVoiture").html("<br><div class=\"alert alert-danger\" align=\"center\">Une erreur est survenue, vous allez être redirigé...</div>");
                 }
             },
-            'text'
+            'text',
          );
     });
 });
 
 
 // Fonction JVS pour changer l'état d'une valeur dans un input (paragraphes, etc... exclus)
-function updateStatut(valuePhp = 'null', idElement = null) 
+function updateStatut(valuePhp = 'null', idElement = null)
 {
 	var temp = valuePhp == "Oui" ? "Non" : "Oui";
 	document.getElementById(idElement).value = temp;
--- View/voitureDetail.php
+++ View/voitureDetail.php
@@ -1,18 +1,22 @@
 <div id="fondBlanc">
-     <hr />
-  <h4 align="center">Voiture: <b><?= $voiture->getMarque()?></b></h4>
-  <hr />
-    <dl class="dl-horizontal">   	
-		<dt>Modele:</dt><dd><?= $voiture->GetModele()?></dd>
-		<dt>Couleur:</dt><dd><?= $voiture->GetCouleur()?></dd>
-		<dt>Actif:</dt><dd><?= $actif = (($voiture->GetActif()>0) ? "Oui" : "Non")?></dd>
-			<?php
-			require 'Utilitaires.php';
+ <hr />
+ <h4 align="center">Voiture: <b><?= $voiture->getMarque()?></b></h4>
+ <hr />
+ <dl class="dl-horizontal">
+  <dt>Modele:</dt><dd><?= $voiture->GetModele()?></dd>
+  <dt>Couleur:</dt><dd><?= $voiture->GetCouleur()?></dd>
+  <dt>Actif:</dt><dd><?= $actif = (($voiture->GetActif()>0) ? "Oui" : "Non")?></dd>
+  <?php
+  require 'Utilitaires.php';
 
-			if($_SESSION['UTILISATEUR_OK'] > 0)
-			{
-				include 'voitureUpdate.php';
-			}?>
-	</dl>
+  if($_SESSION['UTILISATEUR_OK'] > 0)
+  {
+    include 'voitureUpdate.php';
+  }
+  ?>
+  <hr />
+</dl>
+<hr />
 </div>
+
 <script src="../view/script/showModal.js" type="text/javascript" charset="utf-8" async defer></script>
